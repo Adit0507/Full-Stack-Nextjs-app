a4895d104bad67688c228df8e68083fd28822ac1
aditya-singh-skilzen <aditya.kumar@skilzen.com>
2023-06-01 16:25:56 +0530

    Small fiz

--- app/api/prompt/route.js
+++ app/api/prompt/route.js
@@ -11,6 +11,6 @@ export const GET = async (req) => {
             status: 200
         })
     } catch (error) {
-        return new Response("Failed to fetch all prompts", { status: 200 })
+        return new Response("Failed to fetch all prompts", { status: 500 })
     }
 }
